827178d2655b3c68366522e0f33b826d845af73c
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-08-13 17:20:36 +0545

    Add files via upload

---
+++ notOperator.java
@@ -0,0 +1,7 @@
+public class notOperator {
+    public static void main(String[] args) {
+        //LSB & MSB comes into action so the answer shows as -6 instead of 2
+        System.out.println((~5));
+    }
+}
+
