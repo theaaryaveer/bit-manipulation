e74cf3d289fb0bc844bbfbc1847cfdc5ce081a80
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-08-13 17:36:36 +0545

    Add files via upload

---
+++ binaryLeftShift.java
@@ -0,0 +1,11 @@
+public class binaryLeftShift {
+    public static void main(String[] args) {
+        System.out.println((5<<2));
+
+
+        //formula
+
+        // a<<b = a * 2^b
+        // 5<<2 = 5 * 2^2 = 5 * 4 = 20
+    }
+}
