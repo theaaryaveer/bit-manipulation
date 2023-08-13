ccd2daa926771fd67fe48b46731f4fd9f06adc12
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-08-13 17:41:33 +0545

    Add files via upload

---
+++ binaryRightShift.java
@@ -0,0 +1,10 @@
+public class binaryRightShift {
+    public static void main(String[] args) {
+        System.out.println(6>>1);
+
+        //formula
+
+        // a>>b = a/2^b
+        //6>>1 = 6/2^1 = 6/2 = 3
+    }
+}
